cdd36fb074c0806a98ef8138dc7044a6e9f77a3c
Your Name <you@example.com>
2021-07-12 20:49:25 -0300

    feat: Add ks.cfg and pkr.hcl for centos-8

--- builders/virtualbox/linux/centos8/scripts/encrypt.py
+++
@@ -1,6 +0,0 @@
-#from passlib.hash import sha256_crypt as sha256
-from passlib.hash import sha512_crypt as sha512
-
-passwd="password"
-#sha256_passwd = sha256.encrypt(passwd, rounds=5000)
-sha512_passwd = sha512.encrypt(passwd, rounds=5000)
